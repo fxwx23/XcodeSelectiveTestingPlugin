bbfaf7e79da46937259d9bc95eeadb8e81ed1266
fxwx23 <fxwx23@gmail.com>
2024-10-03 16:56:27 +0900

    Update binaryTarget to 0.10.4

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "xcode-selective-test",
-            url: "https://github.com/mikeger/XcodeSelectiveTesting/releases/download/0.9.6/xcode-selective-test.artifactbundle.zip",
-            checksum: "cd434a80775aafa9bc79211f266178f639a90520707b5c418dd265f6fdfd6824"
+            url: "https://github.com/mikeger/XcodeSelectiveTesting/releases/download/0.10.4/xcode-selective-test.artifactbundle.zip",
+            checksum: "ace369a0ef046f0133df70767fb8fadb89f93fccc5302f96439d69cf0058ac19"
         )
     ]
 )
